23d684874ab5263a52ca13716cd0dfe61998e58b
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-23 15:50:47 +0700

    add_link_to_product

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ end
 
 100.times do
   Post.create title: FFaker::Lorem.sentence,
-    body: FFaker::Lorem.paragraphs.join,
+    body: FFaker::Lorem.paragraphs.join * 3,
     tags: Tag.all.sample(rand(10))
 end
